676cb5826dcbdcbf3029d20c83e074fc61a5b5c6
Asma Jaleel <42670333+AsmaJaleel@users.noreply.github.com>
2018-12-18 09:59:25 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,36 @@
+package javaapplication1;
+import java.util.Scanner;
+ 
+/**
+ *
+ * @author MSAJCE
+ */
+public class Main {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+        int n;
+        Scanner s = new Scanner(System.in);
+ 
+        n = s.nextInt();
+        if(n > 0)
+        {
+            System.out.println("Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("Negative");
+        }
+        else
+        {
+            System.out.println(" Zero ");
+        }
+ 
+ 
+    }
+ 
+}
+ 
