b810ddf7cfc26b7cedad0d9716bfa89139026800
Asma Jaleel <42670333+AsmaJaleel@users.noreply.github.com>
2018-12-18 11:14:42 +0530

    Update vc.java

--- vc.java
+++ vc.java
@@ -6,11 +6,11 @@ Scanner in = new Scanner(System. in);
 s=in.next( ).charAt(0);;
 if(s=='a'|| s=='e'|| s=='i'||s=='o'||s=='u')
 {
-	System.out.println("Vowels");
+	System.out.println("Vowel");
 }
 else if(s=='b'|| s=='c'|| s=='d'||s=='f'||s=='g'||s=='h'|| s=='j'|| s=='k'||s=='l'||s=='m'||s=='n'||s=='p'|| s=='q'|| s=='r'||s=='s'||s=='t'||s=='v'|| s=='w'|| s=='x'||s=='y'||s=='z')
 {
-	System.out.println("Consonants");
+	System.out.println("Consonant");
 }
 else
 {
