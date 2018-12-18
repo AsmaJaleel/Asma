45de43b9abf7cf4a107a7281d090598641be764e
Asma Jaleel <42670333+AsmaJaleel@users.noreply.github.com>
2018-12-18 10:08:05 +0530

    Create evod.java

---
+++ evod.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ 
+/**
+ *
+ * @author MSAJCE
+ */
+public class evod {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+        int n;
+        Scanner s = new Scanner(System.in);
+ 
+        n = s.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        
+        else
+        {
+            System.out.println(" Odd");
+        }
+ 
+ 
+    }
+ 
+}
