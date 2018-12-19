046d1e49b817fc5a44a46a161f2bb59a989767ee
Asma Jaleel <42670333+AsmaJaleel@users.noreply.github.com>
2018-12-19 10:48:54 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ public class Hello {
+   public static void main(String[] args){
+       int n,i;
+       Scanner sc=new Scanner(System.in);
+       n=sc.nextInt();
+       if(n!=0){
+       for(i=0;i<n;i++)
+       System.out.println("Hello");
+       }
+       else{
+           return;
+       }
+      
+   }
+}
